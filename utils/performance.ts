8721b7805c58680eb9e0ee7b64dbd33fee5f9b6a
Justiceleeg <justicepwhite@gmail.com>
2025-10-14 21:24:30 -0500

    chore: remove dead code and unused files
    - Remove unused test components (FirebaseTest, RulesTest)
    - Remove stub/placeholder files (SelectionBox, Transformer, usePerformance, performance utils)
    - Remove duplicate VSCode settings file and testing documentation
    - Total: ~410 lines of dead code removed

--- utils/performance.ts
+++
@@ -1,9 +0,0 @@
-// TODO: PR #17 - Performance Monitoring & Optimization
-// Performance utilities
-// - FPS counter
-// - Render time tracking
-
-export const performance = {
-  // TODO: Implement performance utilities
-};
-
